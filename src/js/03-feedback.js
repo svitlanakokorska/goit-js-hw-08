fdbf0544bdfbb4b1318419aa9372c653a3e7dfc5
svitlanakokorska <s.kokorskaya@gmail.com>
2023-03-24 10:32:53 +0200

    правки

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,7 +1,7 @@
 import { throttle } from 'lodash';
 
 const form = document.querySelector('.feedback-form');
-const email = document.querySelector('.input[name="email"]');
+const email = document.querySelector('input[name="email"]');
 const message = document.querySelector('textarea[name="message"]');
 const LOCALSTORAGE_KEY = 'feedback-form-state';
 
